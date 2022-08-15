9e09a4e15fd92dbaf7cb28f15e1d98a70b3705d4
ErikGrootoonk <erik.grootoonk@gmail.com>
2022-08-15 10:36:03 -0400

    update

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt update && upgrade -y
 sudo apt install \
 	curl \
 	neovim\
+	git
 
 
 
@@ -17,3 +18,35 @@ cp ./init.vim  ~/.config/nvim
 
 sh -c 'curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       
+       
+#install virtualbox guest additions
+
+sudo apt install build-essential dkms linux-headers-$(uname -r)
+
+## mount guestadditions cdrom copy contents to a folder
+
+sudo chmod +x VBoxLinuxAdditions.run
+
+## enable share connection with host machine
+
+sudo adduser $USER vboxsf
+
+# install AzureCLI
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+
+#install VSCode
+
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+sudo apt install code
+
+
+
+
+
+
