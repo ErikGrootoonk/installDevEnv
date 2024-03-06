13027540d318443909ed732505efb76f11e61985
Erik Grootoonk <erik.grootoonk@pinkroccade.nl>
2024-03-06 18:54:44 +0100

    6-3-24

--- nvim/wsl-erik/nvim/lua/options.lua
+++ nvim/wsl-erik/nvim/lua/options.lua
@@ -6,10 +6,14 @@ vim.cmd("set clipboard=unnamedplus")
 vim.g.mapleader = ' ' 
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<cr>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<cr>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<cr>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<cr>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<cr>')
 vim.wo.number = true
+vim.wo.relativenumber = true
+vim.keymap.set('n', '<leader>v', '<C-v>')
+vim.keymap.set('n', '<F3>', ':w<cr>')
+vim.keymap.set('i', '<F3>', '<c-o>:w<cr>')
--- nvim/wsl-erik/nvim/lua/plugins/neotree.lua
+++ nvim/wsl-erik/nvim/lua/plugins/neotree.lua
@@ -5,9 +5,11 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
+    "3rd/image.nvim",
 	},
 	config = function()
 		vim.keymap.set("n", "<leader>e", ":Neotree toggle last<CR>", {})
+    	vim.keymap.set("n", "<F2>", ":Neotree toggle last<CR>", {})
 		vim.keymap.set("n", "<leader>ef", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
